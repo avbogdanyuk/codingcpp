00831bfd0b9a38724de64248d49a19ebad7470a4
avbogdanyuk <145556328+avbogdanyuk@users.noreply.github.com>
2023-12-27 16:01:33 +0300

    Create lab3_median.cpp

---
+++ lab3_median.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class MedianFinder
+{
+public:
+    vector <int> arr;
+    
+    void addNum(int num)
+    {
+        arr.push_back(num);
+    }
+    
+    void findMedian()
+    {
+        if ((arr.size()) % 2 == 0)
+        {
+            cout << (arr[arr.size()/2] + arr[(arr.size()/2)-1])/2;
+        }
+        else
+        {
+            cout << arr[arr.size()/2]/2;
+        }
+    }
+};
+
+int main()
+{
+    MedianFinder MF;
+    MF.arr = {};
+    
+    int element;
+    
+    cout << "Enter elements. If you are done write down 000"<<endl;
+    do
+    {
+        cin >> element;
+        MF.addNum(element);
+        
+    } while (element != 000);
+    
+    MF.findMedian();
+}
