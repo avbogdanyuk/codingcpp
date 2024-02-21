66e3d6ccf2936b16a722bf1151d7ed32667da026
avbogdanyuk <145556328+avbogdanyuk@users.noreply.github.com>
2024-02-21 17:18:05 +0300

    Update sem_3.cpp

--- sem_3.cpp
+++ sem_3.cpp
@@ -13,9 +13,11 @@ struct tovar
 void addfirst(tovar* &tlist, string n, float p, int am)
 { //чтобы изменения сохранялись ВНЕ этой функции, нужно использовать ссылку
     tovar* tmp = new tovar;
+    
     tmp -> name = n;
     tmp -> price = p;
     tmp -> amount = am;
+    
     tmp -> next = tlist;
     tlist = tmp;
     return;
@@ -23,10 +25,33 @@ void addfirst(tovar* &tlist, string n, float p, int am)
 
 //добавить товар в список после заданного
 int addafter(tovar* bufet, string n, float p, int am,string nafter)
-{ //последнее -- это имя товара, после которого мы хотим сделать добавление
+{   //последнее -- это имя товара, после которого мы хотим сделать добавление
+    //буфет - это список, в начале он является первым элементом списка
+    
+    tovar* newtovar = new tovar;
+    
+    
+    
     //проверяем,что список не пустой
     if (bufet == NULL) return 1;
     
     tovar* tmp = bufet; //описание с инициализацией
+    while (tmp != NULL && tmp -> name != nafter)
+    {
+        tmp = tmp -> next;
+        if (tmp == NULL) return 1;
+    }
+    
+    newtovar -> name = n;
+    newtovar -> price = p;
+    newtovar -> amount = am;
+    
+    newtovar -> next = tmp -> next;
+    tmp -> next = newtovar;
+    return 0;
+}
+
+int main()
+{
     
 }
