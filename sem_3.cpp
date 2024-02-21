1f374c1db0420e8676c8b15674d1e955b9d5b0ad
avbogdanyuk <145556328+avbogdanyuk@users.noreply.github.com>
2024-02-21 16:51:21 +0300

    Update sem_3.cpp

--- sem_3.cpp
+++ sem_3.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+struct tovar
+{
+    string name;
+    float price;
+    int amount;
+    tovar* next;
+}
+
+void addfirst(tovar* &tlist, string n, float p, int am) //effect elements out off function
+{
+    tovar* tmp = new tovar;
+    tmp -> name = n;
+    tmp -> price = p;
+    tmp -> amount = am;
+    tmp -> next = tlist;
+    tlist = tmp;
+    return;
+}
+
+int main()
+{
+    
+}
