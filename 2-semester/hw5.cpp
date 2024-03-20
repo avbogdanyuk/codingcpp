9a308ce7904d4fe10cc5cc474739dd3ea73d017d
avbogdanyuk <145556328+avbogdanyuk@users.noreply.github.com>
2024-03-20 13:42:54 +0300

    Create hw5.cpp

---
+++ 2-semester/hw5.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+class vect
+{
+    int dim; //размерность вектора
+    double* v; //компоненты класса, массив содержит количество компонентов, pointer
+    int num; //номер вектора
+
+public:
+    static int count; //количество созданных векторов
+
+    //Методы
+
+    //конструкторы
+    vect();
+    vect(int d); //d - размерность векторы, нулевой вектор
+    vect(int d, double* x); //x содержит компоненты вектора
+    vect(vect& x); //конструктор копирования
+
+    //диструктор
+    ~vect();
+
+    //обычные методы класса
+    void print();
+    vect& operator+(vect& right); //компонентная функция класса, левый операнд известен
+    //создаем функцию ДРУГ КЛАССА
+    friend vect operator-(vect l, vect r);
+    vect& operator=(const vect& r);
+};
+
+int vect::count = 0; //НЕОБХОДИМО!
+
+vect::vect(int d)
+{
+    count++; //count = count + 1
+    num = count;
+    cout << "Конструктор vect(int d) создал вектор N" << num;
+    dim = d;
+    v = new double[dim];
+    for (int i = 0; i < dim; i++)
+    {
+        v[i] = 0;
+    }
+}
+
+vect::vect(vect& x)
+{
+    count++; //count = count + 1
+    num = count;
+    cout << "Конструктор vect(vect& x) создал вектор N" << num;
+    dim = x.dim; //razmernost' odinakova teper'
+    v = new double[dim]; //vidilyaem pamyat' pod massive sostoyashoy iz dim amount of double elements
+    for (int i = 0; i < dim; i++)
+    {
+        v[i] = x.v[i]; //copying elements step by step
+    }
+}
+
+vect& vect::operator=(const vect& r)
+{
+    if (dim == 0)
+    {
+        dim = r.dim;
+        v = new double[dim];
+    }
+    for (int i = 0; i < dim; i++)
+    {
+        v[i] = r.v[i];
+    }
+    return *this;
+}
