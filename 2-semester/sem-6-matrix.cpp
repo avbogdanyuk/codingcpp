d6fd1a8294e99b39e3b9089faa5f8f28d298c1d0
avbogdanyuk <145556328+avbogdanyuk@users.noreply.github.com>
2024-04-10 12:21:14 +0300

    Update sem-6-matrix.cpp

--- 2-semester/sem-6-matrix.cpp
+++ 2-semester/sem-6-matrix.cpp
@@ -1,37 +1,34 @@
 #include <iostream>
 using namespace std;
 
-class matr //kvadratnaya matrix
+class matr //квадратная матрица
 {
-    int dim; //razmernost'
-    double* a; //components
-    public:
-    
+    int dim; //размерность
+    double* a; //компененты
+public:
+
     //constuctors
     matr(); //null matrix, a=NULL
-    matr(matr& x); //copying
-    matr(int n); //edinichnaya matrix
-    matr(int n, double* x); //dannie dlya component matrix iz x
-    
+    matr(matr& x); //копирование
+    matr(int n); //единичная матрица
+    matr(int n, double* x); //данные для компонент матрицы из x
+
     //destructor
-    ~matr()
-    {
-        delete a;
-    }
-    
+    ~matr() { delete a; };
+
     //methods
     void print();
     matr operator+(matr& r);
-    matr operator-(matr& r);
-    matr operator-();
-    matr operator*(matr& r); //matrix umnozhaetsya na matrix
-    friend matr operator*(double k, matr& r);
-    //vect operator*(vect& r); //umnozhenie matrix na vector
+    //matr operator-(matr& r);
+    //matr operator-();
+    matr operator*(matr& r); //матрица умножается на матрицу
+    //friend matr operator*(double k, matr& r);
+    //vect operator*(vect& r); //умножение матрицы на вектор
 };
 
 int index(int n, int i, int j)
 {
-    return n*(i-1)+j-1;
+    return n * (i - 1) + j - 1;
 }
 
 matr::matr()
@@ -44,11 +41,13 @@ matr::matr(int n)
 {
     dim = n;
     a = new double[dim];
-    for (int i=1; i<=dim; i++)
-    for (int j=1; j<=dim; j++)
-    {
-        a[index(dim, i, j)] = 1;
-    }
+    for (int i = 1; i <= dim; i++)
+        for (int j = 1; j <= dim; j++)
+        {
+            a[index(dim, i, j)] = 0;
+
+            if (index(dim, i, j)%(dim+1) == 0) a[index(dim, i, j)] = 1;
+        }
 }
 
 matr::matr(matr& x)
@@ -56,40 +55,55 @@ matr::matr(matr& x)
     matr tmp;
     tmp.dim = x.dim;
     tmp.a = new double[dim];
-    for (int i=1; i<=dim; i++)
-    for (int j=1; j<=dim; j++)
-    {
-        tmp.a[index(dim, i, j)] = x.a[index(dim, i, j)];
-    }
+    for (int i = 1; i <= dim; i++)
+        for (int j = 1; j <= dim; j++)
+        {
+            tmp.a[index(dim, i, j)] = x.a[index(dim, i, j)];
+        }
+}
+
+matr::matr(int n, double* x)
+{
+    dim = n;
+    a = new double[n];
+    for (int i = 1; i <= dim; i++)
+        for (int j = 1; j <= dim; j++)
+        {
+                a[index(dim, i, j)] = x[index(dim, i, j)];
+        }
 }
 
 void matr::print()
 {
-    for (int i=1; i<=dim; i++)
-    for (int j=1; j<=dim; j++)
+    for (int i = 1; i <= dim; i++)
     {
-        cout << a[index(dim, i, j)];
+        for (int j = 1; j <= dim; j++)
+        {
+            cout << a[index(dim, i, j)];
+        }
+        cout << endl;
     }
 }
 
+
 matr matr::operator*(matr& r)
 {
-    matr tmp(this->dim); //prosto berem razmernost' left operand, po prikolu
-    
-    for (int i=1; i<=dim; i++)
-    for (int j=1; j<=dim; j++)
-    {
-        tmp.a[index(dim, i, j)] = 0;
-        for (int k=1; k<=dim; k++)
+    matr tmp(this->dim); //берем размерность левого операнда
+
+    for (int i = 1; i <= dim; i++)
+        for (int j = 1; j <= dim; j++)
         {
-            tmp.a[index(dim, i, j)] += a[index(dim, i, k)]*r.a[index(dim, k, j)];
+            tmp.a[index(dim, i, j)] = 0;
+            for (int k = 1; k <= dim; k++)
+            {
+                tmp.a[index(dim, i, j)] += a[index(dim, i, k)] * r.a[index(dim, k, j)];
+            }
         }
-    }
     return tmp;
 }
 
 int main()
 {
-    matr m1(3);
+    matr m1(2);
     m1.print();
 }
